1f92526fa39ae1941317e20cf8df95a75c083cce
briholt100 <brian.c.holt@gmail.com>
2018-01-17 13:40:20 -0800

    script narrative for pulling data out of canvas for R

---
+++ CanvasQuizzes.R
@@ -0,0 +1,8 @@
+#reading canvas quizzes
+
+#open file, read csv
+#identify important columsn
+# remove ID numbers, names, extra junk
+#?sanity checK? original spreadsheet has totals at end of sheet.  Could use those for a quick comparison
+
+
