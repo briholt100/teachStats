c95dd37d07009b0dbe987674e88a90773f30a43f
Holt <Brian.Holt@seattlecolleges.edu>
2018-04-16 15:14:31 -0700

    fixed coin flip...testing needed

--- coinFlip.r
+++ coinFlip.r
@@ -1,15 +1,18 @@
-#coin_flip() does the flipping, two styles, by thumb is standard
-
-coin_flip<-function(type = .5, bias = .1){
-  rbinom(1,1,type+bias) #may need an apply function to apply this to a vector
-}
-mean(replicate(100,coin_flip()))
-
 #coin will be either unbiased or biased.  
+Bag_of_coins<- c(rep(.5,30),rep(.6,70)) #lists fair coins, and  biased coins at .6
 
-Bag_of_coins<- c(rep(.5,9),.6) #lists 9 fair coins, one biased coin at .6
-sample<-sample(Bag_of_coins,5,replace = F) #picks 5 coins randomly from bag
-coin_flip(type=sample,bias=0)
+#coin_flip() does the flipping, two styles, by thumb is standard
+coin_flip<-function(type = .5, bias = .1){
+    rbinom(type,1,type+bias) #may need an apply function to apply this to a vector
+  
+}
+
+coin_flip()
+
+
+sample<-sample(Bag_of_coins,5,replace = T) #picks 5 coins randomly from bag_of_coins
+
+mean(replicate(100,mean(coin_flip(type=sample,bias=0))))
 
 
 
