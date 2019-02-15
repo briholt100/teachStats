68958e52a1583a8c75dbfe1a24f8868fc07fc20a
Holt <Brian.Holt@seattlecolleges.edu>
2019-02-15 15:06:34 -0800

    cleaning up make exam stuff

--- ExamPull.r
+++ ExamPull.r
@@ -1,11 +1,18 @@
 e1<-readClipboard()
 e1[1:6]
-e1<-e1[grep('(^$)',e1,invert=T)]
+e1<-e1[grep('(^$)',e1,invert=T)] #looks for full lines
 e1<-e1[grep("^\\?",e1,invert=T)]
-e1[grep('^[a-e]',e1)]  # only pull answers.  ironically. 
-e1[grep('^[a-e]',e1,invert=T)]
-writeClipboard(e1[grep('^[e]',e1)])
+
+# only pull answers. to find answer 'a', change range in brackets
+e1[grep('^[a-e]',e1,ignore.case = T)]
+
+e1[grep('^[a-e]',e1,ignore.case=T,invert=T)]
+writeClipboard(e1[grep('^[a-e]',e1,ignore.case=T,invert=T)])
 grouping<-1:6
 
 
 
+answer<-readClipboard()
+answers<-sub('^.*\\*([A-E]).*$','\\1',answer)
+
+writeClipboard(answers)
