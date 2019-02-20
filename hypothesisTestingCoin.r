4eea231bbb273ba41248698d18231017318aede2
Holt <Brian.Holt@seattlecolleges.edu>
2019-02-20 14:52:41 -0800

    adding pvalue calculation on simple count of heads

--- hypothesisTestingCoin.r
+++ hypothesisTestingCoin.r
@@ -1,3 +1,4 @@
+library(dplyr)
 exp<-list(5,5)
 obs<-list(3,7)
 out<-as.integer()
@@ -9,24 +10,31 @@ for (i in 1:length(obs)){
 thresh<-sum(out)
 
 
-simulations<-1000
+simulations<-10000
 trial_length<-10
-tally<-matrix(ncol=2,nrow=2)
+tally<-matrix(ncol=3,nrow=2)
 tally<-as.data.frame(tally)
-names(tally)<-c('heads','tails')
+names(tally)<-c('heads','tails','average')
 for(i in 1:simulations){
   trial<-rbinom(trial_length,1,.5)
   #print(trial)
+  average<-mean(trial)
   h<-sum(trial)
   t<-trial_length-h
   #print(i)
   #print(h)
   #print(t)
-  tally[i,]<-(cbind(h,t))
+  tally[i,]<-(cbind(h,t,average))
   
 }
 
 tail(tally)
+pvalue<-length(tally$heads[tally$heads<=3])/nrow(tally)
 
 
-#must count how many breech threshold
+
+
+plot(density(tally$average))
+abline(v=pvalue,col='blue')
+
+
