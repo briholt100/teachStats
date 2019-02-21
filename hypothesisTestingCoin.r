666cb4bc3f976ddae448fa29f89df1acc88ffb15
Holt <Brian.Holt@seattlecolleges.edu>
2019-02-21 14:25:07 -0800

    finished simulation function for coin flip

--- hypothesisTestingCoin.r
+++ hypothesisTestingCoin.r
@@ -1,3 +1,47 @@
+#the following function creates a df of coin tosses of various trials
+coin_toss<-function(sample_size=3,trial_length=10){
+  
+  tally_df<-matrix(ncol=3,nrow=sample_size)
+  tally_df<-as.data.frame(tally_df)
+  names(tally_df)<-c('heads','tails','average')
+  
+    for(i in 1:sample_size){
+      trial<-rbinom(trial_length,1,.5)
+      #print(trial)
+      average<-mean(trial)
+      h<-sum(trial)
+      t<-trial_length-h
+      #print(i)
+      #print(h)
+      #print(t)
+      tally_df[i,]<-(cbind(h,t,average))
+    }
+  return(tally_df)  
+}
+
+coin_toss()
+
+#using coin_toss(), simulate many samples and create a vector of probabilities of 'heads'=3
+simulate_coin_toss<-function(sims=5,sample_size=3,trial_length=10,heads=3){
+  
+  pvalue_df<-vector(length=sims)
+  
+  for (j in 1:sims){ 
+    tally_df<-coin_toss(sample_size = sample_size,trial_length = trial_length)
+      pvalue_df[j]<-length(tally_df$heads[tally_df$heads<=heads])/nrow(tally_df)
+
+  }
+  return(pvalue_df)
+}
+
+pvalue_vec<-simulate_coin_toss(sims=1000,sample_size=100,trial_length=10,heads=5)
+mean(pvalue_vec) #calculates average cumulative probablity of a given number of heads/trials
+hist(pvalue_vec)
+abline(v=mean(pvalue_vec),col='blue')
+abline(v=pbinom(5,10,.5),col='red',lty=3) #plots verticle line of exact cumulative  prob of heads/trials
+
+
+
 library(dplyr)
 exp<-list(5,5)
 obs<-list(2,8)
@@ -10,33 +54,3 @@ for (i in 1:length(obs)){
   #print(out[i])
 }
 thresh<-sum(out)  #sum of squares
-
-
-simulations<-10000
-trial_length<-10
-tally<-matrix(ncol=3,nrow=simulations)
-tally<-as.data.frame(tally)
-names(tally)<-c('heads','tails','average')
-for(i in 1:simulations){
-  trial<-rbinom(trial_length,1,.5)
-  #print(trial)
-  average<-mean(trial)
-  h<-sum(trial)
-  t<-trial_length-h
-  #print(i)
-  #print(h)
-  #print(t)
-  tally[i,]<-(cbind(h,t,average))
-  
-}
-
-tail(tally)
-pvalue<-length(tally$heads[tally$heads<=3])/nrow(tally)
-pvalue
-pbinom(3,10,.5)
-
-
-hist(tally$average)
-abline(v=pvalue,col='blue')
-
-#must count how many breech threshold
